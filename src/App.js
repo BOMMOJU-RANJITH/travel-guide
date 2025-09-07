a813e3261205e8ce8f933d3c77b6a43e95c999b0
BOMMOJU-RANJITH <br484206@gmail.com>
2025-09-07 08:02:08 +0530

    Update App.js
    Removed Redundant code

--- src/App.js
+++ src/App.js
@@ -10,23 +10,7 @@ import Confirmation from './components/confirmation'
 import MyTrips from './components/MyTrips'
 import NotFound from './components/NotFound'
 
-// Note: Use the lists in your code to pass the test cases
-// const stepsList = [
-//   {stepId: 'YOUR_DETAILS', displayText: 'Your Details'},
-//   {stepId: 'DATE_SELECTION', displayText: 'Date Selection'},
-//   {stepId: 'GUESTS', displayText: 'Guests'},
-//   {stepId: 'TRAVEL_ASSISTANCE', displayText: 'Travel Assistance'},
-//   {stepId: 'CONFIRMATION', displayText: 'Confirmation'},
-// ]
 
-// const travelAssistanceList = [
-//   {value: 'car', displayText: 'Car'},
-//   {value: 'flight', displayText: 'Flight'},
-//   {value: 'bus', displayText: 'Bus'},
-//   {value: 'train', displayText: 'Train'},
-// ]
-
-// Replace your code here
 const App = () => (
   <BrowserRouter>
     <Switch>
